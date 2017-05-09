0bb88aab276baf2593ebe161df2021789409d9fe
wborland <wborland@purdue.edu>
2017-05-09 15:56:16 -0500

    First Files

---
+++ questions.js
@@ -0,0 +1,251 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "What was the first year of DCI?": [
+                "1972",
+                "1970",
+                "1974",
+                "1973",
+                "1975",
+                "1976"
+            ]
+        },
+        {
+            "What corps was the first Drum Corps International champion?": [
+                "Anaheim Kingsmen",
+                "Blue Stars",
+                "Santa Clara Vanguard",
+                "27th Lancers",
+                "Troopers",
+                "Kilties"
+            ]
+        },
+        {
+            "What year did the winning score break 90?": [
+                "1975",
+                "1974",
+                "1977",
+                "1978"
+            ]
+        },
+        {
+            "What year was Rodeo first played?": [
+                "1972",
+                "1973",
+                "1985",
+                "1982",
+                "1986"
+            ]
+        },
+        {
+            "Where was the first Drum Corps International world championship?": [
+                "Whitewater, Wisconsin",
+                "Ithava, New York",
+                "Philadelphia, Pennsylvania",
+                "Denver, Colorado"
+            ]
+        },
+        {
+            "When did the Madison Scouts win their first championship?": [
+                "1975",
+                "1973",
+                "1977",
+                "1978"
+            ]
+        },
+        {
+            "What George Hopkins corps won in 1983?": [
+                "Garfield Cadets",
+                "The Cadets",
+                "The Cadets of Bergen County",
+                "Holly name Cadets"
+            ]
+        },
+        {
+            "What musicle was the inspiration for Santa Clara Vanguards 1989 show?": [
+                "Phantom of the Opera",
+                "Cats",
+                "The Wizard of Oz",
+                "Les Misérables"
+            ]
+        },
+        {
+            "Which composer gave Star of Indiana their first win?": [
+                "Respighi",
+                "Copland",
+                "Bernstein",
+                "Shostakovich"
+            ]
+        },
+        {
+            "What non United States city has hosted a Drum Corps International World championships?": [
+                "Montreal",
+                "Vancouver",
+                "Mexico City",
+                "Toronto"
+            ]
+        },
+        {
+            "What Texas city has hosted a Drum Corps International World championships?": [
+                "Dallas",
+                "Houston",
+                "San Antonio",
+                "Austin"
+            ]
+        },
+        {
+            "How many corps have won a championship?": [
+                "10",
+                "11",
+                "9",
+                "12",
+                "8"
+            ]
+        },
+        {
+            "How many times have there been a tie?": [
+                "3",
+                "4",
+                "5",
+                "6"
+            ]
+        },
+        {
+            "What corps has won the most number of championships?": [
+                "Blue Devils",
+                "The Cadets",
+                "The Cavaliers",
+                "Santa Clara Vanguard"
+            ]
+        },
+        {
+            "What is the highest score posted on finals night?": [
+                "99.650",
+                "99.050",
+                "98.750",
+                "99.450"
+            ]
+        },
+        {
+            "What is the western most city to hold a Drum Corps International world championships?": [
+                "Pasadena, California",
+                "Seattle, Washington",
+                "Denver, Colorado",
+                "Vancouver, British Columbia"
+            ]
+        },
+        {
+            "Where was the DCI World championships held when Lucas Oil Stadium was not finished?": [
+                "Bloomington, Indiana",
+                "Michigan City, Indiana",
+                "Whitewater, Wisconsin",
+                "Madison, Wisconsin"
+            ]
+        },
+        {
+            "Which corps has not taken first place at the AlamoDome": [
+                "Carolina Crown",
+                "The Cadets",
+                "Blue Devils",
+                "Bluecoats",
+                "The Cavaliers"
+            ]
+        },
+        {
+            "How many members are allowed in a drum corps?": [
+                "150",
+                "200",
+                "125",
+                "175",
+                "225"
+            ]
+        },
+        {
+            "What is the only corps to qualify for world championships finals every year?": [
+                "Santa Clara Vanguard",
+                "Madison Scouts",
+                "The Cadets",
+                "Blue devils",
+                "Phantom Regiment"
+            ]
+        },
+        {
+            "Which formula was the title of Carolina Crowns 2013 show?": [
+                "e, = mc^2",
+                "V = IR",
+                "torque = r cross f",
+                "F = ma"
+            ]
+        },
+        {
+            "What corps won 5 times between 2000 and 2006?": [
+                "The Cavaliers",
+                "The Cadets",
+                "Blue Devils",
+                "Santa Clara Vanguard"
+            ]
+        },
+        {
+            "What corps has placed in every spot in finals?": [
+                "Madison Scouts",
+                "The Cadets",
+                "Santa Clara Vanguard",
+                "Blue Devils"
+            ]
+        },
+        {
+            "What corps has placed higher at finals night than every other corps without winning a championships?": [
+                "Crossmen",
+                "Blue Knights",
+                "Blue Stars",
+                "Boston Crusaders"
+            ]
+        },
+        {
+            "Which state do the most number of corps call home?": [
+                "California",
+                "Illinois",
+                "New York",
+                "Texas"
+            ]
+        },
+        {
+            "What is the southernmost site to hold a DCI world championship?": [
+                "Miami, Florida",
+                "Orlando, Florida",
+                "Jackson, Mississippi",
+                "Pasadena, California"
+            ]
+        },
+        {
+            "What movie character was the inspiration for the Cavaliers 2004 show?": [
+                "James Bond",
+                "Indiana Jones",
+                "Han Solo",
+                "Forest Gump"
+            ]
+        },
+        {
+            "What Shakespeare play was the inspiration for Phantom Regiments 2011 show?": [
+                "Romeo and Juliet",
+                "Hamlet",
+                "The Tempest",
+                "Macbeth"
+            ]
+        },
+        {
+            "What famous gladiator was the show name for Phantom Regiments 2008 show?": [
+                "Sparticus",
+                "Spiculus",
+                "Hermes",
+                "Commodus"
+            ]
+        }
+    ]
+};
